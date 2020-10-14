b642db9f163728e5d2f17b666149282fc7337921
debatesfights <debatesfight@gmail.com>
2020-10-14 21:10:25 +0300

    Updates

---
+++ precache-manifest.9fda429aefc3a9a1874bd468b0d195cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77be43c34b65f041a095e9ddfe62602",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e4dafebebc15a36cf26",
+    "url": "/static/css/main.9138f908.chunk.css"
+  },
+  {
+    "revision": "6d2ddc4d9c23f5fde846",
+    "url": "/static/js/2.241f32bb.chunk.js"
+  },
+  {
+    "revision": "5c08c41dc83ccd4567cdfdbddc1dbdbc",
+    "url": "/static/js/2.241f32bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4dafebebc15a36cf26",
+    "url": "/static/js/main.9cd60255.chunk.js"
+  },
+  {
+    "revision": "9034ebd0ab725679f497",
+    "url": "/static/js/runtime-main.1f9cc6f9.js"
+  }
+]);
